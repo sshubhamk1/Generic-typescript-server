d194cc98c08606159de94bc4fcbb07ea8b747ee2
Shubham Kumar <sshubhamk1@hotmail.com>
2020-11-25 09:28:14 +0530

    swagger added with duel environment

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, Router, Request, Response } from "express";
 import bodyParser from "body-parser";
-
+import swaggerUI from "swagger-ui-express";
+import swaggerjsDoc from "swagger-jsdoc";
+import cors from "cors";
 // exporting constants
 import { config } from "./Engine/Config/constant";
 
@@ -8,8 +10,36 @@ import { config } from "./Engine/Config/constant";
 const app: Application = express();
 
 // setting up application to accept JSON and urlencoded requests
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// swagger implementation
+const swaggerInfo: swaggerUI.JsonObject = {
+  info: {
+    version: "1.0.0",
+    title: "Generic-typescript Server",
+    description: "Generic typescript server backend APIs",
+    license: "MIT",
+    url: "https://opensource.org/licenses/MIT",
+  },
+  host: `${config.HOST}:${config.PORT}`,
+  basepath: "/",
+};
+let options: any = {
+  swaggerDefinition: swaggerInfo,
+  apis: [
+    `src/app.${config.ENV}`,
+    `src/Public/Routes/*.${config.ENV}`,
+    `src/RoleUser/Routes/*.${config.ENV}`,
+  ],
+  basepath: "/",
+};
+
+const swaggerSpec = swaggerjsDoc(options);
+
+// using swagger at api docs
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 // homepage route
 app.get("/", (req: Request, res: Response) => {
